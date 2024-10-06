426f4a32466916ef193eb41f4c0521108671cefa
Alexander G <alexander.maverick1996@gmail.com>
2024-10-06 20:08:54 -0300

    creating functions

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+// let suma = function(a, b) {
+//     return a + b;
+// };
+
+/*transformación*/
+
+const suma = (a, b) => a + b;
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const ele = document.getElementById("ele1");
+
+function pintar() {
+    ele.style.backgroundColor = 'yellow';
+}
+
+ele.addEventListener("click", pintar);
